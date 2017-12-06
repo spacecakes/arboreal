fb035e45fd28c32cfc9a423b4e95f548e337a4f3
Gabriel <g.lundmark@gmail.com>
2017-12-07 00:30:10 +0100

    Yes!
    Initial push! wooop!

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Fixes menubar to top */
+
+jQuery("document").ready(function($) {
+  const nav = $("#nav-bar");
+  const home = $("#navbar");  
+
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 790) {
+      nav.addClass("nav-bar-fixed");
+    } else {
+      nav.removeClass("nav-bar-fixed");
+    }
+  });
+});
+
+/* Smooth scrolling */
+// Select all links with hashes
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+     }, 500);
+});
