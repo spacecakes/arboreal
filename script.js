6b2c46cd5c8a9e5d44a9bb888b221ceb4a00f963
Gabriel <g.lundmark@gmail.com>
2017-12-12 14:31:05 +0100

    hope this works now

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-<<<<<<< HEAD
 /* Hamburger menu */
 $(".menu-button").click(function(){
-  $(".nav-bar ul").slideToggle('open');
+  $(".nav-bar ul").toggleClass('open');
 });
-=======
-/* Fixes menubar to top 
-No longer needed with position: sticky!
-*/
-
-/* jQuery("document").ready(function($) {
-  const nav = $("#nav-bar");
-  const navOffset = $("#mission");  
-
-
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 1000) {
-      nav.addClass("nav-bar-fixed");
-      navOffset.addClass("nav-bar-offset");
-      
-    } else {
-      nav.removeClass("nav-bar-fixed");
-      navOffset.removeClass("nav-bar-offset");
-      
-    }
-  });
-}); */
->>>>>>> parent of 9ea43a6... hamburger menu for mobile
 
 /* Smooth scrolling */
-// Select all links with hashes
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
